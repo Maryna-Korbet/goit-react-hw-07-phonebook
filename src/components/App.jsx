d7074ef49112475e78462a401582ee530136c554
Maryna-Korbet <m22v86k@gmail.com>
2023-01-05 00:41:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,7 @@ import Filter from "components/Filter/Filter";
 
 export class App extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-
+    contacts: [],
     filter: '',
   };
 
@@ -50,7 +44,7 @@ export class App extends Component {
     const getVisibleContacts = this.getVisibleContacts();
 
     return (
-      <>
+      <div>
         <h1>Phonebook</h1>
         <ContactsForm
           onSubmit={this.onAddContact}
@@ -69,7 +63,7 @@ export class App extends Component {
           filter={filter}
           onDeleteContact={this.onDeleteContact}
         />
-      </>
+      </div>
     );
   }
 };
