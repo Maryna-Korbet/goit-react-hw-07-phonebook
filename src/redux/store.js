716210243f0f6c13bf3d77ae00feafc1f7f61530
Maryna-Korbet <m22v86k@gmail.com>
2023-01-27 20:27:38 +0200

    npm i redux-persist,  added redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+//додаємо слайс import {назва слайса} from 'redux/назва слайса (наприклад import {clicksSlice} from './clickSlice');
+
+const persistConfig ={
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer (
+    persistConfig,
+    //вписуємо редюсери (наприклад clickSlice.reducer)
+)
 
 export const store = configureStore({
-    reducer: {},
+  reducer: {
+    // вписуємо clicks: persistedSliceReducer;
+  },
 });
+
+export const persistor = persistStore(store);
