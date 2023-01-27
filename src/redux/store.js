38572c24b2ca634a3823b4829f22eaae3c435a7e
Maryna-Korbet <m22v86k@gmail.com>
2023-01-27 20:31:45 +0200

    added ignore action

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
+import {
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 //додаємо слайс import {назва слайса} from 'redux/назва слайса (наприклад import {clicksSlice} from './clickSlice');
 
@@ -17,6 +25,12 @@ export const store = configureStore({
   reducer: {
     // вписуємо clicks: persistedSliceReducer;
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
